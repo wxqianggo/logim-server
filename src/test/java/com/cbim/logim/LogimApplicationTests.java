e595fc1c227e937f77f0f7f7753d72b04590d98a
ChrisWang <13133472@qq.com>
2021-02-10 00:21:04 +0800

    Initial commit

---
+++ src/test/java/com/cbim/logim/LogimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbim.logim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
